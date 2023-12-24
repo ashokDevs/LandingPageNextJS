4ca4c5c60523768e8e09dc48b8b3995cd3d9af1b
Ashok Reddy <ashok17748@gmail.com>
2023-12-24 16:52:44 +0530

    BuildChange

--- middlewear.ts
+++
@@ -1,7 +0,0 @@
-import {createMiddlewareClient} from "@supabase/auth-helpers-nextjs"
-import { NextResponse } from "next/server"
-
-export async function middlewear(){
-    const res=NextResponse.next();
-    const supabase = createMiddlewareClient({req,res})
-}
